c00e2c11f5b5e4035500654720f0513ab9acd809
Daniel <daniel.partatus@onet.pl>
2023-10-13 09:23:13 +0200

    planning structure of project

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+
+export const Layout: FC = () => {
+    return(
+        <Layout></Layout>
+    )
+}
---
+++ src/components/roots/Blog.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+
+
+export const Blog: FC =() => {
+    return (
+        <Blog></Blog>
+    )
+}
